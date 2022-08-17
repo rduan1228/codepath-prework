646876c4346fb029b122b83c8e9399f38097eafc
rduan1228 <91286680+rduan1228@users.noreply.github.com>
2022-08-16 20:40:01 -0400

    Added prework files to repository

---
+++ Prework2.0/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Prework2.0
+//
+//  Created by Ran Duan on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.blue
+        
+    }
+    @IBAction func ChangeBackground(_ sender: Any) {
+        
+    }
+    
+    @IBAction func Bye(_ sender: Any) {
+        self.TextLabel.text = "Goodbye!👋";
+
+    }
+
+}
+
